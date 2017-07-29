6302ce3c72260c0b5fe6c461a19faaee7c4af590
NoLevelCap <jemahsam@gmail.com>
2017-07-29 15:02:22 +0100

    PlaceholderRectsOperating

---
+++ game/js/in.js
@@ -0,0 +1,33 @@
+//Create a Pixi stage and renderer and add the
+//renderer.view to the DOM
+var stage = new Container(),
+    renderer = autoDetectRenderer(1280, 960);
+document.body.appendChild(renderer.view);
+
+loader
+  .add("")
+  .load(setup);
+
+//Define any variables that are used in more than one function
+var cat, state = play;
+function setup() {
+  state = play;
+
+  loadMainGame();
+
+  //get the stage details
+  Console.log(stage);
+  //Start the game loop
+  gameLoop();
+}
+function gameLoop(){
+  //Loop this function 60 times per second
+  requestAnimationFrame(gameLoop);
+  //Update the current game state:
+  state();
+  //Render the stage
+  renderer.render(stage);
+}
+
+function play() {
+}
