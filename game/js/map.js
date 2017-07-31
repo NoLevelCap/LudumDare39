cee8e0d0c00b439249d9f3d1b8ddd64efe221539
NoLevelCap <jemahsam@gmail.com>
2017-07-31 01:49:02 +0100

    GET DOWN GIRL GO AHEAD GET DOWN

--- game/js/map.js
+++ game/js/map.js
@@ -85,7 +85,7 @@ function Map(container){
 
     this.map = new Container();
     this.map.x = 640 - 320;
-    this.map.y = 50;
+    this.map.y = 100;
     this.mapContainer.addChild(this.map);
 
     for (var i = 0; i < 32; i++) {
@@ -105,14 +105,9 @@ function Map(container){
     this.generateMap();
     this.createConnections();
 
-    this.repairBtn = new button("Repair", Tex_Main["Button_UI.png"], 10, 10, 144, 48, repairShip);
-    this.mapContainer.addChild(this.repairBtn.Sprite);
-
-    this.buyCrewBtn = new button("Buy crew", Tex_Main["Button_UI.png"], 10, 60, 144, 48, repairShip);
-    this.mapContainer.addChild(this.buyCrewBtn.Sprite);
-
     currentNode = this.segments[0].nodes[0];
     currentNode.setVisted();
+
     this.hideMap();
 }
 
@@ -221,5 +216,6 @@ function Connection(Node1, Node2){
   this.line.position.set(this.line.width/2 + mx*shiftfactor, this.startNode.Sprite.y + my*shiftfactor);
   this.line.rotation = val;
   this.startNode.parent.segment.addChild(this.line);
+
 
 }
